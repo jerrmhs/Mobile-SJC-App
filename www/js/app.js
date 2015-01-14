ebab1c6de954a54a8b13f1843de8b29a8dd862ec
nschleder <nschleder9@gmail.com>
2015-01-14 14:21:48 -0800

    Homepage and Sidebar reworked v2

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
 $(function() {
 	// $.event.special.swipe.scrollSupressionThreshold = 10; // More than this horizontal displacement, and we will suppress scrolling.
-	$.event.special.swipe.horizontalDistanceThreshold = 30; // Swipe horizontal displacement must be more than this.
-	$.event.special.swipe.durationThreshold = 500;  // More time than this, and it isn't a swipe.
-	// $.event.special.swipe.verticalDistanceThreshold = 75; // Swipe vertical displacement must be less than this.
+	$.event.special.swipe.horizontalDistanceThreshold = 25; // Swipe horizontal displacement must be more than this.
+	// $.event.special.swipe.durationThreshold = 500;  // More time than this, and it isn't a swipe.
+	$.event.special.swipe.verticalDistanceThreshold = 75; // Swipe vertical displacement must be less than this.
 	$( "[data-role='header'], [data-role='footer'], [data-role='panel'] " ).toolbar();
 	$("[data-role=panel]").panel().enhanceWithin();
 	$( document ).on( "swiperight", "body", function( e ) {
